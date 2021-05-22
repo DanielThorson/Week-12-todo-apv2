3674d6dc0226428e981d00569173d46aea54ff75
DanielThorson <dragonflydan@gmail.com>
2021-05-21 21:55:17 -0700

    added lots of comments

--- todo-appv2/src/ToDo.js
+++ todo-appv2/src/ToDo.js
@@ -1,6 +1,12 @@
+//This is a component for destructuring the data from the json to a single "todo"
+//which encapsulates each todo with the #1-todo id, #2-task description
+//and #3-a completion value of either true or false.
+//And then giving it a prop of .task and making it return a div with just
+//the task description. It could be "todo.id" to return the id numbers
 const ToDo = ({ todo }) => {
-    console.log(todo);
+  console.log(todo.task);
   return <div>{todo.task}</div>;
 };
 
+//exporting this to be imported into ToDoList
 export default ToDo;
--- todo-appv2/src/ToDoList.js
+++ todo-appv2/src/ToDoList.js
@@ -1,5 +1,9 @@
 import ToDo from "./ToDo.js";
 
+//importing the single Totos further destructuring them and returning a
+//.map over the todo object(task, id and completion value) which is assigned
+//a component for the entire Todo list and exporting as ToDoList.js
+//to import into App.js as a single component
 const ToDoList = ({ toDoList }) => {
   return (
     <div>
